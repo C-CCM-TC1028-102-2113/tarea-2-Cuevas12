a3492493475b0437aedd55893846afa67ef3247d
Cuevas12 <89288980+Cuevas12@users.noreply.github.com>
2021-08-25 10:56:07 -0500

    Update exercise.py

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,1 +1,17 @@
+def main():
+    # Escribe el código adecuado para completar el programa
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    num3 = int(input("Ingresa el tercer número: "))
+
+    if num1 > num2 and num3 < num1:
+        print(num1)
+    elif num2 > num1 and num3 < num2:
+        print(num2)
+    if num3 > num1 and num2 < num3:
+        print(num3)
+    pass
+
+if __name__ == '__main__':
+    main()
 
