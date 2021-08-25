d7ad5e178c3627fdd78e0e95c1bf9bf0acc0ae2a
Cuevas12 <89288980+Cuevas12@users.noreply.github.com>
2021-08-25 10:37:05 -0500

    Update exercise.py

--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,7 +1,39 @@
 def main():
     # Escribe el código adecuado para completar el programa
+    x = int(input("Ingresa el primer número: "))
+    y = int(input("Ingresa el segundo número: "))
+    z = int(input("Ingresa el tercer número: "))
+
+    if x > y and y > z:
+        print(z)
+        print(y)
+        print(x)
+        
+    elif z > y and y > x:
+        print(x)
+        print(y)
+        print(z)
+        
+    elif y > z and z > x:
+        print(x)
+        print(z)
+        print(y)
+        
+    elif y > x and x > z:
+        print(z)
+        print(x)
+        print(y)
+        
+    elif x > z and z > y:
+        print(y)
+        print(z)
+        print(x)
+        
+    elif z > x and x > y:
+        print(y)
+        print(x)
+        print(z)
     pass
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
